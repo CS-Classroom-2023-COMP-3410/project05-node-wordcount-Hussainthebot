22356de1d1bebd2c8182f96dc638e44034fb83e4
Mehboob Hussain <hussain.hussain@du.edu>
2026-02-07 15:58:27 -0700

    updated

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Import required modules
 const fs = require('fs');
 const chalk = require('chalk');
 
@@ -7,7 +6,7 @@ const chalk = require('chalk');
  * @returns {string} The content of the file.
  */
 function readFileContent() {
-    return fs.readFileSync('declaration.txt', 'utf8');
+    return fs.readFileSync('declaration.txt', 'utf8');
 }
 
 /**
@@ -16,17 +15,16 @@ function readFileContent() {
  * @returns {Object} An object with words as keys and their occurrences as values.
  */
 function getWordCounts(content) {
-    const wordCount = {};
-    const words = content
-        .toLowerCase()
-        .split(/\W+/)
-        .filter(Boolean);
+    const wordCount = {};
+    const words = content
+        .toLowerCase()
+        .split(/\W+/)
+        .filter(Boolean);
+    for (const word of words) {
+        wordCount[word] = (wordCount[word] || 0) + 1;
+    }
 
-    for (const word of words) {
-        wordCount[word] = (wordCount[word] || 0) + 1;
-    }
-
-    return wordCount;
+    return wordCount;
 }
 
 /**
@@ -36,13 +34,13 @@ function getWordCounts(content) {
  * @returns {string} The colored word.
  */
 function colorWord(word, count) {
-    if (count === 1) {
-        return chalk.blue(word);
-    } else if (count >= 2 && count <= 5) {
-        return chalk.green(word);
-    } else {
-        return chalk.red(word);
-    }
+    if (count === 1) {
+        return chalk.blue(word);
+    } else if (count >= 2 && count <= 5) {
+        return chalk.green(word);
+    } else {
+    return chalk.red(word);
+    }
 }
 
 /**
@@ -51,38 +49,37 @@ function colorWord(word, count) {
  * @param {Object} wordCount The word occurrences.
  */
 function printColoredLines(content, wordCount) {
-    const lines = content.split('\n').slice(0, 15);
+    const lines = content.split('\n').slice(0, 15);
 
-    for (const line of lines) {
-        const coloredLine =
-            line
-                .split(/\W+/)
-                .filter(Boolean)
-                .map(word => colorWord(word, wordCount[word.toLowerCase()]))
-                .join(' ') + ' ';
-
-        console.log(coloredLine);
-    }
+    for (const line of lines) {
+        const coloredLine =
+            line
+                .split(/\W+/)
+                .filter(Boolean)
+                .map(word => colorWord(word, wordCount[word.toLowerCase()]))
+                .join(' ') + ' ';
+    console.log(coloredLine);
+    }
 }
 
 /**
  * Main function to read the file, count the word occurrences and print the colored lines.
  */
 function processFile() {
-    const content = readFileContent();
-    const wordCount = getWordCounts(content);
-    printColoredLines(content, wordCount);
+    const content = readFileContent();
+    const wordCount = getWordCounts(content);
+    printColoredLines(content, wordCount);
 }
 
 // Only run when executed directly (not during tests)
 if (require.main === module) {
-    processFile();
+    processFile();
 }
 
 // Export functions for testing
 module.exports = {
-    readFileContent,
+    readFileContent,
     getWordCounts,
-    colorWord,
-    printColoredLines
+    colorWord,
+    printColoredLines
 };
